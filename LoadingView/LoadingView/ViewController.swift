e375f22cd46623b70ac4fc2e92cd1b3b8b951ec7
chuangchuang wang <wangch6688@hotmail.com>
2018-04-03 14:00:06 +0800

    create a project
    project init

---
+++ LoadingView/LoadingView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadingView
+//
+//  Created by chuangchuang wang on 2018/4/3.
+//  Copyright © 2018年 chuangchuang wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
