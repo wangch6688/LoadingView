c6d7d5cf16aef31dbe997420b15bb081808b9732
chuangchuang wang <wangch6688@hotmail.com>
2018-04-03 14:04:40 +0800

    create a loading view

---
+++ LoadingView/LoadingView/loadingView/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  LoadingView
+//
+//  Created by chuangchuang wang on 2018/4/3.
+//  Copyright © 2018年 chuangchuang wang. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
